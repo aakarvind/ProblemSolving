1f125f9d0905dee60baf60832f379d9d3339076b
Arvind Kumar Yadav <aakarvind@gmail.com>
2021-01-31 01:16:19 +0530

    kthMinMax

---
+++ KthMaxMin.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class KthMaxMin {
+    static int getKthmin(List<Integer> nums, int k){
+        PriorityQueue<Integer> minHeap=new PriorityQueue<>(nums);
+
+        while(--k>0){
+            minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+    static int getKthMax(Integer []arr,int k){
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->b-a);
+        maxHeap.addAll(Arrays.asList(arr));
+        while(--k>0){
+            maxHeap.poll();
+        }
+        return maxHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n=sc.nextInt();
+        System.out.println();
+        Integer[] nums=new Integer[n];
+//        ArrayList<Integer> nums=new ArrayList<>();
+
+        int i=0;
+        while (i<n)
+        {
+            nums[i]=sc.nextInt();
+            i++;
+        }
+        System.out.print("Enter the value of K: ");
+        int k=sc.nextInt();
+        List<Integer> list= Arrays.asList(nums);
+        System.out.println("Kth Maximum Element: "+ getKthmin(list,k));
+        System.out.println("Kth Maximum Element: "+ getKthMax(nums,k));
+    }
+
+}
