66adf509bf321d98f7fb0b705a643434b06169c7
Arvind Kumar Yadav <aakarvind@gmail.com>
2021-02-06 23:56:05 +0530

    NextPermutation

--- MinimumNumberOfJumps.java
+++ MinimumNumberOfJumps.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MinimumNuberOfJumps {
+public class MinimumNumberOfJumps {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter number of elements: ");
---
+++ NextPermutation.java
@@ -0,0 +1,53 @@
+/*
+ find the first decreasing num a
+ find the smallest num that is greater than a, and swap it with a
+ reverse the right part of index i
+*/
+
+import java.util.Scanner;
+
+public class NextPermutation {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n=sc.nextInt();
+        System.out.println();
+        int []nums=new int[n];
+        int i=0;
+        while (i<n) {
+            nums[i]=sc.nextInt();
+            i++;
+        }
+
+        int []result=nextPermutation(nums);
+        System.out.println("Merged Intervals:");
+        for (int arr:result){
+            System.out.print(arr+" ");
+        }
+    }
+    public static int[] nextPermutation(int []nums){
+        int right= nums.length-2;
+        while(right>=0 && nums[right+1]<=nums[right]){
+            right--;
+        }
+        if(right>=0) {
+            int justGreater = nums.length - 1;
+            while (justGreater>0 && nums[justGreater]<=nums[right]){
+                justGreater--;
+            }
+            int temp=nums[justGreater];
+            nums[justGreater]=nums[right];
+            nums[right]=temp;
+        }
+        int left=right+1;
+        right= nums.length-1;
+        while(left<=right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;right--;
+
+        }
+        return nums;
+    }
+}
